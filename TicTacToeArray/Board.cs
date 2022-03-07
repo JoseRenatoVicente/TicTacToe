32d877f4b58c8919d31b28fb87f06a627c520620
José Vicente <josetq12@gmail.com>
2022-03-07 18:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ TicTacToeArray/Board.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace JogoVelhaMatriz
+{
+    internal class Board
+    {
+        TicTacToeService ticTacToeService = new TicTacToeService();
+
+        public void InputPosition()
+        {
+            Console.WriteLine("Vez do Jogador " +
+                (ticTacToeService.currentPlayer == 'X' ? ticTacToeService.Player1 : ticTacToeService.Player2));
+
+            Console.WriteLine("Digite a posição");
+            if (!char.TryParse(Console.ReadLine(), out char position))
+            {
+                Console.WriteLine("Posição inválida");
+                InputPosition();
+            }
+
+            if (ticTacToeService.AddPosition(position))
+                ShowBoard();
+            else
+                ShowWinner();
+        }
+
+        public void ShowWinner()
+        {
+            int status = ticTacToeService.CheckEarned();
+
+            if (status > 0 && status < 3)
+                Console.WriteLine($"O jogador {(status == 1 ? ticTacToeService.Player1 : ticTacToeService.Player1)} ganhou");
+            else if (status == 0)
+                Console.WriteLine("Deu velha!");
+
+            ticTacToeService.board = ticTacToeService.ResetBoard();
+        }
+
+        public void ShowBoard()
+        {
+            Console.Clear();
+
+            for (int row = 0; row < ticTacToeService.board.GetLength(0); row++)
+            {
+                for (int column = 0; column < ticTacToeService.board.GetLength(1); column++)
+                {
+                    Console.Write($" \t|{ticTacToeService.board[row, column]}|");
+                }
+                Console.WriteLine();
+                Console.WriteLine("\t-------------------");
+            }
+            InputPosition();
+        }
+
+        public void InputPlayer()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o nome do Jogador 1 (X)");
+            ticTacToeService.Player1 = Console.ReadLine() + "(X)";
+
+            Console.WriteLine("Digite o nome do Jogador 2 (O)");
+            ticTacToeService.Player2 = Console.ReadLine() + "(O)";
+
+            ShowBoard();
+        }
+
+
+        public void Menu()
+        {
+
+            Console.WriteLine(@"
+       dP                                     dP                               dP dP                
+       88                                     88                               88 88                
+       88 .d8888b. .d8888b. .d8888b.    .d888b88 .d8888b.    dP   .dP .d8888b. 88 88d888b. .d8888b. 
+       88 88'  `88 88'  `88 88'  `88    88'  `88 88'  `88    88   d8' 88ooood8 88 88'  `88 88'  `88 
+88.  .d8P 88.  .88 88.  .88 88.  .88    88.  .88 88.  .88    88 .88'  88.  ... 88 88    88 88.  .88 
+ `Y8888'  `88888P' `8888P88 `88888P'    `88888P8 `88888P8    8888P'   `88888P' dP dP    dP `88888P8 
+                    ~~~~.88
+                    d8888P                      
+
+                                1) Iniciar o jogo
+                                ------------------------------
+                                0) - Sair
+");
+
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "0": Environment.Exit(0); break;
+
+                case "1":
+                    InputPlayer();
+                    BackMenu();
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida! ");
+                    BackMenu();
+                    break;
+            }
+
+        }
+
+        public void BackMenu()
+        {
+            Console.WriteLine("\n Pressione qualquer tecla para voltar ao menu...");
+            Console.ReadKey();
+            Console.Clear();
+            Menu();
+        }
+    }
+}
---
+++ TicTacToeArray/TicTacToeService.cs
@@ -0,0 +1,80 @@
+﻿namespace JogoVelhaMatriz
+{
+
+    internal class TicTacToeService
+    {
+        public char[,] board = new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+        public char currentPlayer = 'X';
+        public string Player1, Player2;
+
+        public bool AddPosition(char position)
+        {
+            for (int row = 0; row < board.GetLength(0); row++)
+                for (int column = 0; column < board.GetLength(1); column++)
+                    if (board[row, column] == position)
+                    {
+                        board[row, column] = currentPlayer;
+                        currentPlayer = InvertPlayer();
+                    }
+            
+            if (CheckEarned() != 3)
+                return false;
+
+            return true;
+        }
+
+        public int CheckEarned()
+        {
+            string result = "";
+
+            //CheckMainDiagonal
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                result += board[row, row];
+
+                if (result == "XXX")
+                    return 1;
+                else if (result == "OOO")
+                    return 2;
+            }
+
+            //CheckSecondaryDiagonal
+            result = "";
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                result += board[row, board.GetLength(0) - 1 - row];
+
+                if (result == "XXX")
+                    return 1;
+                else if (result == "OOO")
+                    return 2;
+            }
+
+            //CheckRowsColumns
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                string rows = $"{board[i, 0]}{board[i, 1]}{board[i, 2]}";
+
+                string columns = $"{board[0, i]}{board[1, i]}{board[2, i]}";
+
+                if (rows == "XXX" || columns == "XXX")
+                    return 1;
+                else if (rows == "OOO" || columns == "OOO")
+                    return 2;
+            }
+
+            //CheckTie
+            for (int row = 0; row < board.GetLength(0); row++)
+                for (int column = 0; column < board.GetLength(1); column++)
+                    if (board[row, column] == ResetBoard()[row, column])
+                        return 3;
+
+            return 0;
+        }
+
+
+        public char InvertPlayer() => currentPlayer == 'X' ? 'O' : 'X';
+        public char[,] ResetBoard() => new char[3, 3] { { '1', '2', '3' }, { '4', '5', '6' }, { '7', '8', '9' } };
+
+    }
+}
